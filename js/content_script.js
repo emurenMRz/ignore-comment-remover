4d446e698bdfcd1fbb7ab631bb061b3ee7df8743
emurenMRz <emuren@mrz-net.org>
2021-12-28 19:16:08 +0900

    add support: local ignore list

--- js/content_script.js
+++ js/content_script.js
@@ -1,35 +1,132 @@
 const comentList = document.getElementById('cmtlst');
-let hideIgnoreList = true;
 
-function setIgnoreList(state) {
-	hideIgnoreList = state;
-	updateIgnoreList();
+/**
+ * 公式の無視リスト関係の処理
+ */
+
+class IgnoreList {
+	static hide = true;
+
+	static setState(state) {
+		IgnoreList.hide = state;
+		IgnoreList.update();
+	}
+
+	static update(node) {
+		if (!node) node = comentList;
+		if (!('querySelectorAll' in node)) return;
+
+		const ignoreComment = node.querySelectorAll('.ignoreComment');
+		if (ignoreComment.length > 0) {
+			for (const comment of ignoreComment)
+				comment.classList.toggle('ICR-hideCommonList', IgnoreList.hide);
+			console.info(`「無視リスト対象です。」を${IgnoreList.hide ? '除去' : '表示'}しました。`);
+		}
+	}
 }
 
-function updateIgnoreList(node) {
-	if (!node) node = comentList;
-	if (!('querySelectorAll' in node)) return;
+/**
+ * ローカル無視リスト関係の処理
+ */
 
-	const ignoreComment = node.querySelectorAll('.ignoreComment');
-	const ignoreCommentQuantity = ignoreComment.length
-	for (const comment of ignoreComment)
-		comment.classList.toggle('ICR-hideCommonList', hideIgnoreList);
-	if (ignoreCommentQuantity > 0)
-		console.info(`「無視リスト対象です。」を${hideIgnoreList ? '除去' : '表示'}しました。`);
+class LocalIgnoreList {
+	static ignoreUsers = new Set;
+
+	static extractWriterData(comment) {
+		const writer = comment.querySelector('.comWriter');
+		if (!writer)
+			return null;
+		const a = writer.firstElementChild;
+		return { name: a.lastChild.data, id: a.dataset.user };
+	}
+
+	static initialize() {
+		for (const comment of comentList.querySelectorAll('div.comment')) {
+			const data = LocalIgnoreList.extractWriterData(comment);
+			if (!data)
+				continue;
+			const menuList = comment.querySelector('.comMenu>.comMenuList');
+			if (menuList)
+				menuList.appendChild(LocalIgnoreList.buildMenuItem(data));
+		}
+	}
+
+	static setUsers(users) {
+		if (users.constructor.name != 'Array')
+			return;
+		LocalIgnoreList.ignoreUsers = new Set(users);
+		LocalIgnoreList.update();
+	}
+
+	static updateUser(userId, state) {
+		if (state)
+			LocalIgnoreList.ignoreUsers.add(userId);
+		else
+			LocalIgnoreList.ignoreUsers.delete(userId);
+		LocalIgnoreList.update();
+	}
+
+	static update(node) {
+		chrome.storage.local.set({ ignoreUsers: [...LocalIgnoreList.ignoreUsers] }, () => { });
+
+		if (!node) node = comentList;
+		for (const comment of node.querySelectorAll('div.comment')) {
+			const data = LocalIgnoreList.extractWriterData(comment);
+			if (!data)
+				continue;
+			const menuList = comment.querySelector('.comMenu>.comMenuList');
+			if (menuList && menuList.lastElementChild?.dataset?.type != 'add-ignore-user')
+				menuList.appendChild(LocalIgnoreList.buildMenuItem(data));
+			comment.classList.toggle('ICR-hideExtendedList', LocalIgnoreList.ignoreUsers.has(data.id));
+		}
+	}
+
+	static buildMenuItem(data) {
+		const a = document.createElement('a');
+		a.textContent = '無視する';
+		a.href = 'javascript:void(0)';
+		a.dataset.name = data.name;
+		a.dataset.userId = data.id;
+		a.addEventListener('click', LocalIgnoreList.appendIgnoreUser);
+		const li = document.createElement('li');
+		li.dataset.type = 'add-ignore-user';
+		li.appendChild(a);
+		return li;
+	}
+
+	static appendIgnoreUser() {
+		const userId = this.dataset.userId;
+		if (LocalIgnoreList.ignoreUsers.has(userId))
+			return;
+		LocalIgnoreList.ignoreUsers.add(userId);
+		LocalIgnoreList.update();
+	}
 }
 
-function commentRemover(mutationList, observer) {
+/**
+ * ページを開いた際の初期化処理
+ */
+
+LocalIgnoreList.initialize();
+
+chrome.storage.local.get({ hideIgnoreList: true, ignoreUsers: [] }, result => {
+	IgnoreList.setState(result.hideIgnoreList);
+	LocalIgnoreList.setUsers(result.ignoreUsers);
+});
+
+const observer = new MutationObserver((mutationList, observer) => {
 	mutationList.forEach(mutation => {
 		if (mutation.type == 'childList')
-			for (const node of mutation.addedNodes)
-				updateIgnoreList(node);
+			for (const node of mutation.addedNodes) {
+				IgnoreList.update(node);
+				LocalIgnoreList.update(node);
+			}
 	});
-}
-
-chrome.storage.local.get({ hideIgnoreList: true }, result => setIgnoreList(result.hideIgnoreList));
-
-const observer = new MutationObserver(commentRemover);
+});
 observer.observe(comentList, { childList: true });
 addEventListener('beforeunload', () => observer.disconnect());
 
-window.ICR = { setIgnoreList: setIgnoreList };
+window.ICR = {
+	setIgnoreList: IgnoreList.setState,
+	updateIgnoreUser: LocalIgnoreList.updateUser
+};
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,14 @@
+/**
+ * 公式の無視リスト関係の処理
+ */
+
 const hideIgnoreList = document.getElementById('hideIgnoreList');
+
+hideIgnoreList.addEventListener('change', function () {
+	const state = this.checked;
+	sendIgnoreListState(state);
+	chrome.storage.local.set({ hideIgnoreList: state }, () => { });
+});
 
 function sendIgnoreListState(state) {
 	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
@@ -6,17 +16,53 @@ function sendIgnoreListState(state) {
 			target: { tabId: tabs[0].id },
 			func: state => ICR.setIgnoreList(state),
 			args: [state]
-		}, r => console.info(r));
+		}, () => { });
 	});
 }
 
-chrome.storage.local.get({ hideIgnoreList: true }, result => {
-	hideIgnoreList.checked = result.hideIgnoreList;
-	sendIgnoreListState(result.hideIgnoreList);
-});
+/**
+ * ローカル無視リスト関係の処理
+ */
 
-hideIgnoreList.addEventListener('change', function () {
-	const state = this.checked;
-	sendIgnoreListState(state);
-	chrome.storage.local.set({ hideIgnoreList: state }, () => { });
+const userList = document.getElementById('userList');
+
+function buildIDItem(userId) {
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.id = userId;
+	checkbox.checked = true;
+	checkbox.addEventListener('change', function () {
+		const userId = this.id;
+		const state = this.checked;
+		chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+			chrome.scripting.executeScript({
+				target: { tabId: tabs[0].id },
+				func: (userId, state) => ICR.updateIgnoreUser(userId, state),
+				args: [userId, state]
+			}, () => { });
+		});
+	});
+
+	const label = document.createElement('label');
+	label.appendChild(checkbox);
+	label.appendChild(document.createTextNode(userId));
+	return label;
+}
+
+/**
+ * ページを開いた際の初期化処理
+ */
+chrome.storage.local.get({ hideIgnoreList: true, ignoreUsers: [] }, result => {
+	hideIgnoreList.checked = result.hideIgnoreList;
+	if (result.ignoreUsers.constructor.name == 'Array') {
+		if (result.ignoreUsers.length > 0) {
+			for (const user of result.ignoreUsers)
+				userList.appendChild(buildIDItem(user));
+		} else {
+			const span = document.createElement('span');
+			span.className = 'noEntry';
+			span.textContent = '誰も登録されていません。';
+			userList.appendChild(span);
+		}
+	}
 });
